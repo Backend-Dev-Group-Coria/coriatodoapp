4c97e021b8c1f7442912eb7bd832702b497254d5
Miroslav Zadravec <miro@miroz.com.hr>
2022-11-23 10:00:09 +0100

    HeartBeat api

---
+++ backend/CoriaToDo/CoriaToDo.API/Feature/Health/HeartBeatController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoriaToDo.API.Feature.Health;
+[Route("api/[controller]")]
+[ApiController]
+public class HeartBeatController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "It's alive!";
+    }
+}
--- backend/CoriaToDo/CoriaToDo.API/Program.cs
+++ backend/CoriaToDo/CoriaToDo.API/Program.cs
@@ -1,7 +1,7 @@
 
 namespace CoriaToDo.API
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
