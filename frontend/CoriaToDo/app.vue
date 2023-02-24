74a68dcb8f36fc27d219faff90eed8ca2ed0578c
abelic <abelic@coria.com>
2023-02-24 12:30:00 +0100

    Composition api for app.vue component.

--- frontend/CoriaToDo/app.vue
+++ frontend/CoriaToDo/app.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
     <h1>CoriaToDo App</h1>
-    <ul>
+    <ul v-if="todoItems">
       <li v-for="item in todoItems" :key="item.id">{{ item.title }}</li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-const { data: login } = await useFetch<any>("http://localhost:5067/api/auth/login/0",{method: 'POST'});
-const { data: todoItems } = useFetch<any>("http://localhost:5067/api/Todo",);
-console.log(todoItems);
+import { Ref, ref, onMounted } from 'vue';
+
+const todoItems: Ref<any> = ref([])
+
+onMounted(() => {
+  login(1)
+  getToDoItems()
+})
+
+async function login(id: number) {
+  await useFetch<any>(`http://localhost:5067/api/auth/login/${id}`, {method: 'POST'});
+}
+
+async function getToDoItems() {
+  const { data: items } = useFetch<any>("http://localhost:5067/api/Todo",);
+  todoItems.value = items.value
+}
 </script>
