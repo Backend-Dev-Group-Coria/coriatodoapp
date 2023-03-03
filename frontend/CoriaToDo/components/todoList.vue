2af7a6401746e45df5d73ac170adf0abbdc6574f
Bruno Krusic <bkrusic@coria.com>
2023-03-03 12:19:43 +0100

    Added input field.

--- frontend/CoriaToDo/components/todoList.vue
+++ frontend/CoriaToDo/components/todoList.vue
@@ -4,6 +4,7 @@
     <ul v-if="todoItems && todoItems.length">
       <li v-for="item in todoItems" :key="item.id" :class="{ underline: item.completed }">{{ item.title }}</li>
     </ul>
+    <input v-model="newTodoItemTitle" placeholder="Add new..." @input="onInput" @keyup.enter="onEnter" />
   </div>
 </template>
 
@@ -13,10 +14,22 @@ import { TodoItem } from '../models/todo-item';
 import todoApi from '../api/todo.api'
 
 const todoItems: Ref<TodoItem[]> = ref([])
+const newTodoItemTitle: Ref<string> = ref("")
+
 
 onMounted(async () => {
   todoItems.value = await todoApi.getToDoItems()
 })
+
+function onInput()
+{
+  console.log(newTodoItemTitle.value)
+}
+
+function onEnter()
+{
+  console.log('Miro je pritisnul enter.')
+}
 
 </script>
 
